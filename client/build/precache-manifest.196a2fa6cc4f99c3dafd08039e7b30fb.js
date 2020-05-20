a61c076a7397c38cb28ca3330466b5444b178afa
VirajRG <viraj.gawde123@gmail.com>
2020-05-20 10:58:24 +0530

    build

---
+++ client/build/precache-manifest.196a2fa6cc4f99c3dafd08039e7b30fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d077895f6375368acedcb9c55d5dca0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89dd51c09cdab9afc114",
+    "url": "/static/css/main.531f45d2.chunk.css"
+  },
+  {
+    "revision": "ba32280266cbdd650a91",
+    "url": "/static/js/2.d842a364.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.d842a364.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89dd51c09cdab9afc114",
+    "url": "/static/js/main.3d7c8aa8.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
