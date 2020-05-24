34a0c5c4ab54f473299264ae85759ed7b89965b7
VirajRG <viraj.gawde123@gmail.com>
2020-05-24 22:26:14 +0530

    working on filters

--- client/src/components/Home/tasks.jsx
+++ client/src/components/Home/tasks.jsx
@@ -20,26 +20,34 @@ class Tasks extends React.Component {
 			selectedStatus: [],
 			todos: this.props.todos
 		};
-		this.handleFilter = this.handleFilter.bind(this);
+		// this.handleFilter = this.handleFilter.bind(this);
+		// this.searchChange = this.searchChange.bind(this);
+		// this.filterDate = this.filterDate.bind(this);
+		// this.filterTxt = this.filterTxt.bind(this);
+
 	}
-	handleFilter = (e, type) => {
-		var search = '', date = [];
-		if (type == 'search') {
-			var search = e.target.value;
-			console.log(search, type);
-			this.setState({ searchText: search });
-			// if(search!=='') {
-			// 	var data = this.props.todos.filter(function(todo) {
-			// 		return todo.name.toLowerCase().includes(search.toLowerCase());
-			// 	});
-			// 	this.setState({todos: data});
-			// }
-			// else 
-			// 	this.setState({todos:this.props.todos});
+	filterTxt  = (name) => {
+		if ( this.state.searchText != '') {
+			console.log(name.toLowerCase().includes(this.state.searchText.toLowerCase()), "hi");
+			return name.toLowerCase().includes(this.state.searchText.toLowerCase());
 		}
-		else if (type == 'date') {
-			
-		}
+		else 
+			return true;
+	}
+	filterDate = (due) => {
+		if(this.state.rangeDate[0]!=null)
+			moment(due).isSameOrAfter(this.state.rangeDate[0].format('YYYY-MM-DD')) && moment(due).isSameOrBefore(this.state.rangeDate[1].format('YYYY-MM-DD'));
+		else
+			return true;
+	}
+	handleFilter = () => {
+		var propData = this.props.todos;
+		var the = this;
+		var data = propData.filter(function(todo) {
+			return this.filterTxt(todo.name) && this.filterDate(todo.due);
+		});
+		console.log(data);
+		this.setState({todos: data});
 	}
 	handleSelect = (type, value) => {
 		console.log(type, value);
@@ -50,14 +58,25 @@ class Tasks extends React.Component {
 			this.setState({ selectedStatus: value });
 		}
 	}
+	searchChange = e => {
+		var name = e.target.name;
+		this.setState({[name]:e.target.value});
+		setTimeout(() => {
+			this.handleFilter();
+		}, 100);
+	}
 	dateChange = (date, dateString) => {
 		if (dateString[0] !== "") {
 			var date = moment(dateString[0], 'YYYY-MM-DD');
 			this.setState({ rangeDate: [moment(dateString[0], 'YYYY-MM-DD'), moment(dateString[1], 'YYYY-MM-DD')] });
+			// this.setState({rangeDate: dateString});
 		}
-		else
-			this.setState({ rangeDate: [null, null] })
+		else {
+			this.setState({ rangeDate: [null, null] });
+		}
+		setTimeout(() => {
 			this.handleFilter('', 'date');
+		}, 100);
 	}
 	render() {
 		const Label = this.props.labels.map(function (l) {
@@ -72,7 +91,7 @@ class Tasks extends React.Component {
 					<div className="filters">
 						<Button type="primary">Add Todo</Button>
 						<br /><br />
-						<Input size="large" suffix={<SearchOutlined />} name="searchTxt" value={this.state.searchTxt} onChange={(e) => this.handleFilter(e, 'search')} className="search-input" placeholder="Search Todo" />
+						<Input size="large" suffix={<SearchOutlined />} name="searchTxt" value={this.state.searchTxt} onChange={(e)=>this.searchChange(e)} className="search-input" placeholder="Search Todo" />
 						<br /><br />
 						<RangePicker onChange={this.dateChange} size="large" value={this.state.rangeDate} />
 						<br /><br />
@@ -110,7 +129,7 @@ class Tasks extends React.Component {
 						renderThumbVertical={props => <div {...props} className="thumb-vertical" style={{ display: "none" }} />}
 					>
 						{this.state.todos.map(function (todo) {
-							return <div className="card" style={todo.priority == 'high' ? { borderTop: '5px solid #cf1322' } : todo.priority == 'normal' ? { borderTop: '5px solid #006d75' } : { borderTop: '5px solid #5b8c00' }} title={todo.name}>
+							return <div key={todo.id} className="card" style={todo.priority == 'high' ? { borderTop: '5px solid #cf1322' } : todo.priority == 'normal' ? { borderTop: '5px solid #006d75' } : { borderTop: '5px solid #5b8c00' }} title={todo.name}>
 								<div className="card-row">
 									<p className="card-title">{todo.name}</p>
 									<div className="card-badge">
@@ -121,12 +140,12 @@ class Tasks extends React.Component {
 								<div className="card-row">
 									<p className="card-desc">{todo.desc}</p>
 									<div className="card-badge">
-										{todo.label.map(function (l) {
-											return <Tag>{l}</Tag>
+										{todo.label.map(function (l, key) {
+											return <Tag key={key}>{l}</Tag>
 										})}
 									</div>
 								</div>
-								<div style={{textAlign:'left'}}>{todo.due}</div>
+								<div style={{ textAlign: 'left' }}>{todo.due}</div>
 							</div>
 						})}
 					</Scrollbars>
