8b7601795765dce02dd594fddd9201ef14a1e02e
VirajRG <viraj.gawde123@gmail.com>
2020-05-30 23:00:33 +0530

    optimised

--- server/index.js
+++ server/index.js
@@ -1,30 +1,3 @@
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const path = require('path');
-// var MongoClient = require('mongodb').MongoClient;
-
-// const app = express();
-// app.use(bodyParser.urlencoded({ extended: true }));
-// // app.use(bodyParser.json());
-// // app.use(express.static(path.join(__dirname, '../client/build')));
-
-// // MongoClient.connect('mongodb://localhost:27017/root', function (err, client) {
-// // 	if (err) throw err;
-// // 	var db = client.db('techzilla');
-// // 	db.collection('todo').find().toArray(function (err, result) {
-// // 		if (err) throw err;
-// // 		console.log(result);
-// // 	})
-// // })
-// app.post('/login', function (request, response) {
-// 	console.log(request.body);
-// 	response.send("Hello there!!");
-// });
-// // app.get('/', function (req, res) {
-// // 	res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-// // });
-
-// app.listen(process.env.PORT || 8080);
 require('dotenv').config();
 const express = require('express')
 const bodyParser = require('body-parser')
